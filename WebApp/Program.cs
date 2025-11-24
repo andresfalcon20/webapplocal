1b1e9328ceb51873977443f494c63481a565765c
andresfalcon20 <andresfalcon.18@hotmail.com>
2025-11-24 16:40:35 -0500

    ss
    Removed DbContext configuration and updated default route.

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,17 +1,18 @@
-using Microsoft.EntityFrameworkCore;
-using WebApp.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<CrudCoreContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("App"));
-});
+
+// No necesitas DbContext ni repositorios
+// builder.Services.AddDbContext<CrudCoreContext>(...);
+// builder.Services.AddSingleton<IPeopleRepository, InMemoryPeopleRepository>();
+
 var app = builder.Build();
 
- 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -25,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=People}/{action=Index}/{id?}");
 
 app.Run();
